aea23fe49c27fe743b0f9aacc7bd725ac4b951b6
Zamrony P. Juhara <zamronypj@yahoo.com>
2017-04-26 19:44:05 +0800

    add db config implementation

---
+++ src/DbConfig.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Juhara\Queue\Db;
+
+use Juhara\Queue\Db\DbConfigInterface;
+
+class DbConfig implements DbConfigInterface {
+    private $dbHost = 'localhost';
+    private $dbName = 'queuedb';
+    private $username = 'juhara';
+    private $password = 'juhara';
+    private $tableName = 'queue';
+
+    /**
+     * Set host of database (IP or hostname)
+     * @param string $host
+     */
+    public function setHost($host) {
+        $this->dbHost = $host;
+    }
+
+    /**
+     * get host of database
+     * @return string
+     */
+    public function getHost() {
+        return $this->dbHost;
+    }
+
+    /**
+     * Set database name
+     * @param string $dbName
+     */
+    public function setDbName($dbName) {
+        $this->dbName = $dbName;
+    }
+
+    /**
+     * get database name
+     * @return string
+     */
+    public function getDbName() {
+        return $this->dbName;
+    }
+
+    /**
+     * Set username
+     * @param string $userName
+     */
+    public function setUserName($userName) {
+         $this->username = $userName;
+    }
+
+    /**
+     * get username
+     * @return string
+     */
+    public function getUserName() {
+        return $this->username;
+    }
+
+    /**
+     * Set user password
+     * @param string $userPassword
+     */
+    public function setUserPassword($userPassword) {
+         $this->password = $userPassword;
+    }
+
+    /**
+     * get userpassword
+     * @return string
+     */
+    public function getUserPassword() {
+        return $this->password;
+    }
+
+    /**
+     * Set table name to store queue
+     * @param string $tableName
+     */
+    public function setTableName($tableName) {
+        $this->tableName = $tableName;
+    }
+
+    /**
+     * get table name to store queue
+     * @return string
+     */
+    public function getTableName() {
+        return $this->tableName;
+    }
+
+}
